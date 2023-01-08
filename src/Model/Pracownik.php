dfcf6434dbb9844687e3e9041de1d63e70a3432c
xEveryth1ngx <dobrowolski.piotrof@gmail.com>
2023-01-08 20:51:40 +0100

    fixed a mistake in pracownik model

--- src/Model/Pracownik.php
+++ src/Model/Pracownik.php
@@ -125,7 +125,7 @@ class Movie
         return $Pracownicy;
     }
 
-    public static function find($id): ?Movie
+    public static function find($id): ?self
     {
         $pdo = new \PDO(Config::get('db_dsn'), Config::get('db_user'), Config::get('db_pass'));
         $sql = 'SELECT * FROM pracownik WHERE id = :id';
