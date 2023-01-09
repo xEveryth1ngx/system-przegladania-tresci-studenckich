7da020a6a68cf7e7f6d3e8cfae0b528f5f06f1c5
patix2222 <104532722+patix2222@users.noreply.github.com>
2023-01-09 20:53:37 +0100

    dziala

--- templates/base.html.php
+++ templates/base.html.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 
     <title>ZUT</title>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="/assets/dist/style.min.css">
 </head>
+
 <body>
     <div class="nav">
         <img src="/assets/images/zutlogo.jpg">
@@ -22,7 +24,26 @@
             </div>
         </div>
 
-       
+        <div class="right">
+            <select name="building" id="building-select">
+                <option selected value="WI1P1">WI1 piętro 1</option>
+                <option value="WI1P2">WI1 piętro 2</option>
+                <option value="WI1P3">WI1 piętro 3</option>
+                <option value="WI2P1">WI2 piętro 1</option>
+                <option value="WI2P2">WI2 piętro 2</option>
+                <option value="WI2P3">WI2 piętro 3</option>
+            </select>
+            <div class="map">map placeholder</div>
+            <div class="info">
+                Link do planu zajęć: <a href="">Plan.com</a><br>
+                Tytuł:<br>
+                Imię:<br>
+                Nazwisko:<br>
+                Gdzie może przebywać:
+            </div>
+        </div>
+
     </div>
 </body>
+
 </html>
